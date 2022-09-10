f751c828617a5273927abc2e18079fff1ac3e21d
Udam <udamjoe8@gmail.com>
2022-09-10 09:15:51 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,19 +4,16 @@
 /**
  *  * Description: main - Print out a string to stdout.
  *   * Return: 0 if successfull.
- *    */
+*/
 int main(void)
 {
 		int n;
 			srand(time(0));
-
 				n = rand() - RAND_MAX / 2;
-
-				if (n > 0)
+					if (n > 0)
 								printf("%d is positive\n", n);
 						else if (n == 0)
 									printf("%d is zero\n", n);
-
 							else
 										printf("%d is negative\n", n);
 								return (0);
