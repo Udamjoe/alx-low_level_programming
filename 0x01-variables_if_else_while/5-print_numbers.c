68e8035141587eba7da2c8405d94ca0a279ac2e4
Udam <udamjoe8@gmail.com>
2022-09-11 09:56:25 -0700

    frog

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
-* main - print if the number is postive, zero, or negative
-*
-* Description: using the main function
-* this program prints "Programming is positive, zero, or negative
-* Return: 0
-*/
+ * Description: main - Print out a string to stdout.
+ * Return: 0 if successfull.
+ */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
 }
